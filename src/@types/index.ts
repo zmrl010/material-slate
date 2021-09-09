6de6f075829894083c7592493aae18fa5df19aac
Zach <zmriel@gmail.com>
2021-09-09 16:39:41 -0400

    more fixes to get custom types working in host packages

--- src/@types/index.ts
+++ src/@types/index.ts
@@ -1,4 +1,4 @@
-import { CustomEditor, CustomText, CustomElement } from "./types";
+import { CustomEditor, CustomText, CustomElement } from "../lib/types";
 
 declare module "slate" {
   interface CustomTypes {
