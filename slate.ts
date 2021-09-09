10159e9b70dd53f87ae831cb5a4ba5bb66ca6749
Zach <zmriel@gmail.com>
2021-09-09 12:37:28 -0400

    remove slate module in root dir and set rootDir in tsconfig

--- slate.ts
+++
@@ -1,9 +0,0 @@
-/**
- * @module
- * re-exports slate vars under material-slate/slate
- * @todo find a better way to do this
- */
-
-export * from "slate";
-export * from "slate-react";
-export * from "slate-history";
